026599dd07cac17441a262b59cf76e9b1144b279
nikita <wsr7@K218PC9>
2022-05-17 12:50:42 +0300

    изменение название окна

--- Covid19/ViewModels/MainWindowViewModel.cs
+++ Covid19/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Covid19.ViewModels.Base;
 
 namespace Covid19.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        private string _Title = "Анализ статистики Cv19";
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title, value);
+        }
     }
 }
